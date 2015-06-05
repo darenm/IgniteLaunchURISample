7568b10f6e02812f2d4b497c1281277492804110
Daren May <daren.may@gmail.com>
2015-06-04 20:07:49 -0600

    Added the correct handling of the OnNavigated method

--- IgniteApp2/App.xaml.cs
+++ IgniteApp2/App.xaml.cs
@@ -119,5 +119,45 @@ namespace IgniteApp2
               Current.Id.FamilyName;
         }
 
+        protected override void OnActivated(IActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                var protocolArgs = e as ProtocolActivatedEventArgs;
+                if (protocolArgs != null)
+                {
+                    rootFrame.Navigate(typeof(MainPage), protocolArgs.Uri);
+                }
+                else
+                {
+                    rootFrame.Navigate(typeof(MainPage));
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
     }
 }
